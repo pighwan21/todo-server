0aff86ea0fa7bf455f8b4cabeb7169ff41fbd30b
pighwan21 <pighwan21@gmail.com>
2022-12-20 16:33:22 +0900

    테스트 코드 작성

---
+++ src/test/java/org/example/service/TodoServiceTest.java
@@ -0,0 +1,78 @@
+package org.example.service;
+
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
+import org.example.repository.TodoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceTest {
+
+    @Mock
+    private TodoRepository todoRepository;
+
+    @InjectMocks
+    private TodoService todoService;
+    // Mock을 사용하는 이유는 크게 두 가지
+    // 1. 외부 시스템에 의존하지 않고 자체 테스트를 실행할 수 있어야함.
+    // 2. 실제 데이터베이스를 연결하여 사용하게 되면 테스트를 할 때마다 db에 변경이 이루어질 수 있기 때문
+    @Test
+    void add() {
+        when(this.todoRepository.save(any(TodoEntity.class)))
+                .then(AdditionalAnswers.returnsFirstArg());
+
+        TodoRequest expected = new TodoRequest();
+        expected.setTitle("Test Title");
+
+        TodoEntity actual = this.todoService.add(expected);
+
+        assertEquals(expected.getTitle(), actual.getTitle());   // 넣은 값과 실제로 반환 받은 값의 동일성을 확인하기 위해
+    }
+
+    @Test
+    void searchById() {
+        TodoEntity entity = new TodoEntity();
+        entity.setId(123L);
+        entity.setTitle("TITLE");
+        entity.setOrder(0L);
+        entity.setCompleted(false);
+        Optional<TodoEntity> optional = Optional.of(entity);
+
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(optional);
+
+        TodoEntity actual = this.todoService.searchById(123L);
+
+        TodoEntity expected = optional.get();
+
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getTitle(), actual.getTitle());
+        assertEquals(expected.getOrder(), actual.getOrder());
+        assertEquals(expected.getCompleted(), actual.getCompleted());
+    }
+
+    // 에러를 잘 발생시키는 것도 테스트의 중요한 요소 중 하나.
+    @Test
+    public void searchByIdFailed() {
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> {
+            this.todoService.searchById(123L);
+        });
+    }
+}
